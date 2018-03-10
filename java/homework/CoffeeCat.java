731361f8c0b3b03d9bf7304791f702db9b739904
eason <492760446@qq.com>
2018-03-10 22:51:15 +0800

    提交了接口相关内容

---
+++ java/homework/CoffeeCat.java
@@ -0,0 +1,63 @@
+class CoffeeCat {
+	public static void main(String[] args) {
+		Cat c = new Cat("¼Ó·Æ",8);
+		c.eat();
+		c.sleep();
+		System.out.println(c.getAge());
+		JumpCat jc = new JumpCat("Ìø¸ßÃ¨",3);
+		jc.eat();
+		jc.sleep();
+		jc.jump();
+	}
+}
+abstract class Animal {
+	private String name;
+	private int age;
+	public Animal(){}
+	public Animal(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public int getAge(){
+		return age;
+	}
+	public abstract void eat();
+	public abstract void sleep();
+}
+
+interface Jumping {
+	public void jump();
+}
+
+class Cat extends Animal {
+	public Cat() {}
+	public Cat(String name,int age) {
+		super(name,age);
+	}
+	public void eat() {
+		System.out.println("Ã¨³ÔÓã");
+	}
+	public void sleep() {
+		System.out.println("²à×ÅË¯");
+	}
+}
+
+class JumpCat extends Cat implements Jumping {
+	public JumpCat(){}
+	public JumpCat(String name,int age) {
+		super(name,age);
+	}
+	public void jump (){
+		System.out.println("Ã¨Ìø¸ß");
+	}
+}
+
